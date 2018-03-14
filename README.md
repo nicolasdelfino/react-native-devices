fa3df0bc8c8dbe4812b8c785078e31f4fd4a28d1
nicolasdelfino <nicolas.delfino@tretton37.com>
2018-03-14 20:15:01 +0100

    feat: iPads for everyone

--- README.md
+++ README.md
@@ -47,15 +47,20 @@ const top = device.IS_X ? 100 : 0
 
 ### Helpers
 
-| Functions        | ...        |
-| ---------------- | ---------- |
-| IF_X             | if, if not |
-| IF_PLUS_6_6S_7_8 | if, if not |
-| IF_6_6S_7_8      | if, if not |
-| IF_5_5S_5C_SE    | if, if not |
-| IF_4_4S          | if, if not |
-| IF_2G_3G_3GS     | if, if not |
-| IF_TABLET        | if, if not |
+| Functions        | arguments  | comments                     |
+| ---------------- | ---------- | ---------------------------- |
+| IF_X             | if, if not |                              |
+| IF_PLUS_6_6S_7_8 | if, if not |                              |
+| IF_6_6S_7_8      | if, if not |                              |
+| IF_5_5S_5C_SE    | if, if not |                              |
+| IF_4_4S          | if, if not |                              |
+| IF_2G_3G_3GS     | if, if not |                              |
+| IF_TABLET        | if, if not |                              |
+| IF_IPAD          | if, if not | (Mini, 1st & 2nd Generation) |
+| IF_IPAD_MINI     | if, if not | (2nd, 3rd & 4th Generation)  |
+| IF_IPAD_AIR      | if, if not | (1st & 2nd Generation)       |
+| IF_IPAD_RETINA   | if, if not | (3rd & 4th Generation)       |
+| IF_IPAD_PRO      | if, if not |                              |
 
 | Bools            |
 | ---------------- |
@@ -66,7 +71,13 @@ const top = device.IS_X ? 100 : 0
 | IS_4_4S          |
 | IS_2G_3G_3GS     |
 | IS_TABLET        |
+| IS_IPAD          |
+| IS_IPAD_MINI     |
+| IS_IPAD_AIR      |
+| IS_IPAD_RETINA   |
+| IS_IPAD_PRO      |
 
 ### Why?
 
-There are others out there (like the inspiration for RND - react-native-iphone-x-helper). Difference is this one gives you more stuff.
+There are others out there (like the syntax inspiration for RND - react-native-iphone-x-helper).<br>
+Difference is this one gives you more stuff.
--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const h = Dimensions.get('window').height * r
 /*
   PHONES
 */
-const DEVICE_IS_X = () => w === 1125 && h === 2436
-const DEVICE_IS_PLUS_6_6S_7_8 = () => w === 1242 && h === 2208
-const DEVICE_IS_6_6S_7_8 = () => w === 750 && h === 1334
-const DEVICE_IS_5_5S_5C_SE = () => w === 640 && h === 1136
-const DEVICE_IS_4_4S = () => w === 640 && h === 960
-const DEVICE_IS_2G_3G_3GS = () => w === 320 && h === 480
+const DEVICE_IS_X = () => verifyProfile(1125, 2436)
+const DEVICE_IS_PLUS_6_6S_7_8 = () => verifyProfile(1242, 2208)
+const DEVICE_IS_6_6S_7_8 = () => verifyProfile(750, 1334)
+const DEVICE_IS_5_5S_5C_SE = () => verifyProfile(640, 1136)
+const DEVICE_IS_4_4S = () => verifyProfile(640, 960)
+const DEVICE_IS_2G_3G_3GS = () => verifyProfile(320, 480)
 
 const DEVICE_IF_X = (a, b) => (DEVICE_IS_X() ? a : b)
 const DEVICE_IF_PLUS_6_6S_7_8 = (a, b) => (DEVICE_IS_PLUS_6_6S_7_8() ? a : b)
@@ -29,7 +29,23 @@ const DEVICE_IF_2G_3G_3GS = (a, b) => (DEVICE_IS_2G_3G_3GS() ? a : b)
 */
 const DEVICE_IS_TABLET = () =>
   (r < 2 && (w >= 1000 || h >= 1000)) || (r === 2 && (w >= 1920 || h >= 1920))
+const DEVICE_IS_IPAD_MINI = () => verifyProfile(1536, 2048)
+const DEVICE_IS_IPAD = () => verifyProfile(768, 1024)
+const DEVICE_IS_IPAD_PRO = () => verifyProfile(2048, 2732)
+const DEVICE_IS_IPAD_AIR = () => verifyProfile(1536, 2048)
+const DEVICE_IS_IPAD_RETINA = () => verifyProfile(1536, 2048)
+
 const DEVICE_IF_TABLET = (a, b) => (DEVICE_IS_TABLET() ? a : b)
+const DEVICE_IF_IPAD_MINI = (a, b) => (DEVICE_IS_IPAD_MINI() ? a : b)
+const DEVICE_IF_IPAD = (a, b) => (DEVICE_IS_IPAD() ? a : b)
+const DEVICE_IF_IPAD_PRO = (a, b) => (DEVICE_IS_IPAD_PRO() ? a : b)
+const DEVICE_IF_IPAD_AIR = (a, b) => (DEVICE_IS_IPAD_AIR() ? a : b)
+const DEVICE_IF_IPAD_RETINA = (a, b) => (DEVICE_IS_IPAD_RETINA() ? a : b)
+
+/*
+  VERIFY PIXEL PROFILE (portrait / landscape)
+*/
+const verifyProfile = (a, b) => (w === a && h === b) || (h === a && w === b)
 
 export const devices = {
   IF_X: (a, b) => DEVICE_IF_X(a, b),
@@ -39,11 +55,21 @@ export const devices = {
   IF_4_4S: (a, b) => DEVICE_IF_4_4S(a, b),
   IF_2G_3G_3GS: (a, b) => DEVICE_IF_2G_3G_3GS(a, b),
   IF_TABLET: (a, b) => DEVICE_IF_TABLET(a, b),
+  IF_IPAD_MINI: (a, b) => DEVICE_IF_IPAD_MINI(a, b),
+  IF_IPAD: (a, b) => DEVICE_IF_IPAD(a, b),
+  IF_IPAD_PRO: (a, b) => DEVICE_IF_IPAD_PRO(a, b),
+  IF_IPAD_AIR: (a, b) => DEVICE_IF_IPAD_AIR(a, b),
+  IF_IPAD_RETINA: (a, b) => DEVICE_IF_IPAD_RETINA(a, b),
   IS_X: DEVICE_IS_X(),
   IS_PLUS_6_6S_7_8: DEVICE_IS_PLUS_6_6S_7_8(),
   IS_6_6S_7_8: DEVICE_IS_6_6S_7_8(),
   IS_5_5S_5C_SE: DEVICE_IS_5_5S_5C_SE(),
   IS_4_4S: DEVICE_IS_4_4S(),
   IS_2G_3G_3GS: DEVICE_IS_2G_3G_3GS(),
-  IS_TABLET: DEVICE_IS_TABLET()
+  IS_TABLET: DEVICE_IS_TABLET(),
+  IS_IPAD_MINI: DEVICE_IS_IPAD_MINI(),
+  IS_IPAD: DEVICE_IS_IPAD(),
+  IS_IPAD_PRO: DEVICE_IS_IPAD_PRO(),
+  IS_IPAD_AIR: DEVICE_IS_IPAD_AIR(),
+  IS_IPAD_RETINA: DEVICE_IS_IPAD_RETINA()
 }
